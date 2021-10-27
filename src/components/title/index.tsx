4793693ce77a1d02c3d7377f39eefc9fb57e54b5
Bruno Silva <brs.rsilva@gmail.com>
2021-10-27 08:12:00 -0300

    change readme

---
+++ src/components/title/index.tsx
@@ -0,0 +1,26 @@
+// packages
+import React from 'react'
+
+// styles
+import style from './style.module.scss'
+
+interface IProps {
+  title: string
+  subtitle?: string
+  description?: string
+}
+
+const Title: React.FC<IProps> = ({ title, subtitle, description }) => {
+  return (
+    <div className={style.container}>
+      <div className={style.option1}>
+        <span className={style.title}>
+          {title} <strong>{subtitle}</strong>
+        </span>
+        <span className={style.description}>{description}</span>
+      </div>
+    </div>
+  )
+}
+
+export default Title
