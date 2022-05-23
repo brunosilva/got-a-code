5802bc9cf81f489b387b8bcce3ce70aa67a560b5
Bruno Silva <brs.rsilva@gmail.com>
2022-05-23 08:07:53 -0300

    refactor: names

--- src/components/Organization/utils.ts
+++ src/components/Organization/utils.ts
@@ -9,16 +9,16 @@ export const dataUser: ITreeUser = {
     { 
       id: 'n2', 
       level: 1, 
-      name: 'Bo Miao', 
+      name: 'Blz blz', 
       title: 'department manager'
     },
     {
       id: 'n3',
       level: 1,
-      name: 'Su Miao',
+      name: 'Tô suave',
       title: 'department manager',
       children: [
-        { id: 'n4', level: 2, name: 'Tie Hua', title: 'senior engineer' },
+        { id: 'n4', level: 2, name: 'Tia Hua', title: 'senior engineer' },
         {
           id: 'n5',
           level: 2,
@@ -26,24 +26,24 @@ export const dataUser: ITreeUser = {
           title: 'senior engineer',
           children: [
             { id: 'n6', name: 'Dan Dan', title: 'engineer' },
-            { id: 'n7', name: 'Xiang Xiang', title: 'engineer' }
+            { id: 'n7', name: 'Zas zas', title: 'engineer' }
           ]
         },
-        { id: 'n8', level: 2, name: 'Pang Pang', title: 'senior engineer' }
+        { id: 'n8', level: 2, name: 'Ping Pong', title: 'senior engineer' }
       ]
     },
     { 
       id: 'n9', 
       level: 1, 
-      name: 'Hong Miao', 
+      name: 'Miao miao', 
       title: 'department manager' 
     },
     {
       id: 'n10',
       level: 1,
-      name: 'Chun Miao',
+      name: 'Au au',
       title: 'department manager',
-      children: [{ id: 'n11', name: 'Yue Yue', title: 'senior engineer' }]
+      children: [{ id: 'n11', name: 'Yeah Yeah', title: 'senior engineer' }]
     }
   ]
 }
